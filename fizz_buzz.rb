a9a317e550ccc45e68efcf92c2ad0ec1eef56132
Alex Romney <alexromney1215@gmail.com>
2017-04-08 16:47:32 -0600

    added user input capability

--- fizz_buzz.rb
+++ fizz_buzz.rb
@@ -1,15 +1,20 @@
 require 'pry'
 require 'colorize'
 
-puts "Here is a list of 100 numbers.".colorize(:green); sleep 2
+puts "Alright here is the scoop!".colorize(:green); sleep 1.5
 puts "For every number divisible by 3 the word 'Fizz' will print to the screen".colorize(:red); sleep 3
 puts "For every number divisible by 5 the word 'Buzz' will print out"; sleep 3
 puts "And for every number divisble by 3 and 5 the word 'FIZZBUZZ' will pop out.".colorize(:light_blue); sleep 3
-puts "It's as simple as that!".colorize(:yellow); sleep 2
+puts "It's as simple as that!"; sleep 2
 puts "So let's get started!".colorize(:green); sleep 2
 
-def fizzbuzz(n)
-  (1..n).each do |i|
+puts "Please enter a number:".colorize(:yellow)
+num = gets.strip.to_i
+puts "Your number is #{num}".colorize(:yellow); sleep 1
+puts "\nHere we go!\n".colorize(:green); sleep 1
+
+def fizzbuzz(num)
+  (1..num).each do |i|
     if i % 15 == 0
       puts "FIZZBUZZ".colorize(:light_blue)
     elsif i % 5 == 0
@@ -22,4 +27,4 @@ def fizzbuzz(n)
   end
 end
 
-fizzbuzz(100)
+fizzbuzz(num)
